a859d1b725000bf3a945fd4a37524362997fab48
imprathamraj <rjpratham05@gmail.com>
2025-02-21 21:51:04 +0530

    [Pratham Raj] Add. Open and Run the Hello World App from VS Code

--- src/main/java/com/springapp/controller/Controller.java
+++ src/main/java/com/springapp/controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
 
-    // UC1
+    // UC3 Added Spring Boot Extension Pack
     @GetMapping("/hello")
     public String hello(){
         return "Hello form BridgeLabz";
