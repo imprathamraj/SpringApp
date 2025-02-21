0c07ed7981ebc0803e9d28970f72c0a2bff99d83
imprathamraj <rjpratham05@gmail.com>
2025-02-21 18:12:33 +0530

    [Pratham Raj] Add. Create a Hello world Spring App in a Web Page

---
+++ src/main/java/com/springapp/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.springapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class WebController {
+
+    // UC2
+    @GetMapping("/web")
+    public String hello(Model model){
+        model.addAttribute("hello","Hello from Pratham");
+        return "hello";
+    }
+    @GetMapping("/web/message")
+    public String message(Model model){
+        model.addAttribute("message", "Hello form BridgeLabz");
+        return "message";
+    }
+}
